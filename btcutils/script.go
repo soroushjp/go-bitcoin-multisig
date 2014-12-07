a152437de5109e04ee14a2806c1b18632cab25de
Soroush Pour <me@soroushjp.com>
2014-12-07 23:52:46 +1100

    Added script.go to add Bitcoin Script OP codes as enum for code readability

---
+++ btcutils/script.go
@@ -0,0 +1,30 @@
+package btcutils
+
+const (
+	OP_1 = 81 + iota
+	OP_2
+	OP_3
+	OP_4
+	OP_5
+	OP_6
+	OP_7
+	OP_8
+	OP_9
+	OP_10
+	OP_11
+	OP_12
+	OP_13
+	OP_14
+	OP_15
+	OP_16
+)
+
+const (
+	OP_0             = 0
+	OP_DUP           = 118
+	OP_EQUAL         = 135
+	OP_EQUALVERIFY   = 136
+	OP_HASH160       = 169
+	OP_CHECKSIG      = 172
+	OP_CHECKMULTISIG = 174
+)
